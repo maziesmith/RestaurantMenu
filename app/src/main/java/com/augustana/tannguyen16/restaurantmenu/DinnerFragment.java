a4ef30176e0666b0f8aae214ddbe307524fa0925
Nguyen <tannguyen16@augustana.edu>
2018-03-20 15:19:03 -0500

    Initial files create

---
+++ app/src/main/java/com/augustana/tannguyen16/restaurantmenu/DinnerFragment.java
@@ -0,0 +1,19 @@
+package com.augustana.tannguyen16.restaurantmenu;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by tannguyen16 on 3/20/2018.
+ */
+
+public class DinnerFragment extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        return inflater.inflate(R.layout.fragment_dinner, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/augustana/tannguyen16/restaurantmenu/MainActivity.java
@@ -0,0 +1,37 @@
+package com.augustana.tannguyen16.restaurantmenu;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    private static final String TAB_KEY_INDEX = "tab_key";
+    private Fragment breakfastFragment;
+    private Fragment lunchFragment;
+    private Fragment dinnerFragment;
+    private Fragment snackFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        actionBar.setDisplayShowTitleEnabled(false);
+
+        ActionBar.Tab breakfastTab = actionBar.newTab().setText(getString(R.string.ui_tabname_breakfast));
+        ActionBar.Tab lunchTab = actionBar.newTab().setText(getString(R.string.ui_tabname_lunch));
+        ActionBar.Tab snackTab = actionBar.newTab().setText(getString(R.string.ui_tabname_snack));
+        ActionBar.Tab dinnerTab = actionBar.newTab().setText(getString(R.string.ui_tabname_dinner));
+
+        breakfastFragment = new BreakfastFragment();
+        snackFragment = new SnackFragment();
+        lunchFragment = new LunchFragment();
+        dinnerFragment = new DinnerFragment();
+    }
+
+}
