a0205ed6615ddf4ec39f31ce1359a68879723a48
tannguyen16 <nguyendinhtan96@gmail.com>
2018-03-20 21:28:24 -0500

    Finish the CheckoutActivity and FileI/O

--- app/src/main/java/com/augustana/tannguyen16/restaurantmenu/CheckoutActivity.java
+++ app/src/main/java/com/augustana/tannguyen16/restaurantmenu/CheckoutActivity.java
@@ -23,22 +23,56 @@ import java.io.InputStreamReader;
 
 public class CheckoutActivity extends Activity {
     private TextView appetizer;
+    private TextView entree;
+    private TextView dessert;
+
+    private String[] appetizerList = {"Glazed Chicken Wing\n$5.00", "Honey-Garlic Meatball\n$4.00", "Avocado Salsa\n$3.00"};
+    private String[] entreeList = {"Chili Braised Beef Noodle Bowl\n$5.00", "Fried Chicken Livers with Marinated Figs\n$4.00", "Beef Short Ribs Braised in Red Wine\n$3.00"};
+    private String[] dessertList = {"Chantal's New York Cheesecake\n$5.00", "Chocolate Lovin Spooncake\n$4.00", "Classic Oreo Icebox\n$3.00"};
+
+    private TextView totalCost;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.checkout_activity);
 
-        String x = readFromFile(CheckoutActivity.this);
+        String appetizerChange = readFromFile(CheckoutActivity.this, "appetizer.txt");
+        String entreeChange = readFromFile(CheckoutActivity.this, "entree.txt");
+        String dessertChange = readFromFile(CheckoutActivity.this, "dessert.txt");
+
         appetizer = (TextView) findViewById(R.id.textView3);
-        appetizer.setText(x);
+        entree = (TextView) findViewById(R.id.textView4);
+        dessert = (TextView) findViewById(R.id.textView5);
+        totalCost = (TextView) findViewById(R.id.textView7);
+
+        Integer num1 = Integer.parseInt(appetizerChange) - 1;
+        Integer num2 = Integer.parseInt(entreeChange) - 1;
+        Integer num3 = Integer.parseInt(dessertChange) - 1;
+        appetizer.setText(appetizerList[num1]);
+        entree.setText(entreeList[num2]);
+        dessert.setText(dessertList[num3]);
+
+        Integer total = computeCost(num1+1) + computeCost(num2+1) + computeCost(num3+1);
+        totalCost.setText("$"+total+".00");
 
     }
-
-    private String readFromFile(Context context) {
+    private Integer computeCost(Integer number){
+        switch(number){
+            case 1:
+                return 5;
+            case 2:
+                return 4;
+            case 3:
+                return 3;
+            default:
+                return 0;
+        }
+    }
+    private String readFromFile(Context context, String name) {
 
         String ret = "";
 
         try {
-            InputStream inputStream = context.openFileInput("appetizer.txt");
+            InputStream inputStream = context.openFileInput(name);
 
             if ( inputStream != null ) {
                 InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
--- app/src/main/java/com/augustana/tannguyen16/restaurantmenu/EntreeFragment.java
+++ app/src/main/java/com/augustana/tannguyen16/restaurantmenu/EntreeFragment.java
@@ -1,19 +1,59 @@
 package com.augustana.tannguyen16.restaurantmenu;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 /**
  * Created by tannguyen16 on 3/20/2018.
  */
 
 public class EntreeFragment extends Fragment{
+    View myView;
+    private RadioButton radioButton;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        return inflater.inflate(R.layout.fragment_entree, container, false);
+        myView =  inflater.inflate(R.layout.fragment_entree, container, false);
+        RadioGroup radioGroup = (RadioGroup) myView .findViewById(R.id.radioGroup);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
+        {
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                // checkedId is the RadioButton selected
+                switch(checkedId) {
+                    case R.id.radioButton:
+                        writeToFile("1", getActivity());
+                        break;
+                    case R.id.radioButton2:
+                        writeToFile("2", getActivity());
+                        break;
+                    case R.id.radioButton3:
+                        writeToFile("3", getActivity());
+                        break;
+                }
+            }
+        });
+        return myView;
+    }
+    private void writeToFile(String data, Context context) {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("entree.txt", Context.MODE_PRIVATE));
+            outputStreamWriter.write(data);
+            outputStreamWriter.close();
+            Log.d("Success", "Written to file");
+        }
+        catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
     }
 
 }
